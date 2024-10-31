f6c46cc450fddac1fece46f26008b3c64b68ee7d
Владимир Владимирович Путин! <144924225+PutinVVV@users.noreply.github.com>
2024-10-31 18:41:51 +0300

    Шурыгин Сергей. Задача №1. Вариант №16. Нахождение максимальных значений по столбцам матрицы (#33)
    Код выполняет вычисление максимальных значений в каждом столбце матрицы.
    Матрица представлена как двухмерный вектор целых чисел
    **(std::vector<std::vector<int>>)**.
    
    **Описание последовательной задачи**
    Каждый элемент матрицы сравнивается с текущим максимальным значением в
    соответствующем столбце, и при нахождении большего значения оно
    сохраняется как новое максимальное значение. В конце выполнения функции
    возвращается вектор, содержащий максимальные значения для каждого
    столбца.
    
    **Описание MPI задачи**
    Матрица целых чисел разбивается на равные части. Каждый процесс получает
    свою часть матрицы и находит максимальные значения в каждом столбце этой
    части. Затем все процессы отправляют свои результаты на главный процесс,
    который собирает их и определяет окончательные максимальные значения для
    каждого столбца всей матрицы.
    
    ---------
    
    Co-authored-by: Шурыгин Сергей <Shurigin11@mail.ru>

---
+++ tasks/mpi/Shurygin_S_max_po_stolbam_matrix/include/ops_mpi.hpp
@@ -0,0 +1,47 @@
+﻿// Copyright 2023 Nesterov Alexander
+#pragma once
+#include <gtest/gtest.h>
+
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <memory>
+#include <numeric>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace Shurygin_S_max_po_stolbam_matrix_mpi {
+
+class TestMPITaskSequential : public ppc::core::Task {
+ public:
+  explicit TestMPITaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+  static std::vector<int> generate_random_vector(int size, int lower_bound = 0, int upper_bound = 50);
+  static std::vector<std::vector<int>> generate_random_matrix(int rows, int columns);
+
+ private:
+  std::vector<std::vector<int>> input_;
+  std::vector<int> res_;
+};
+
+class TestMPITaskParallel : public ppc::core::Task {
+ public:
+  explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<std::vector<int>> input_;
+  std::vector<std::vector<int>> local_input_;
+  std::vector<int> res_;
+  boost::mpi::communicator world;
+};
+
+}  // namespace Shurygin_S_max_po_stolbam_matrix_mpi
---
+++ tasks/seq/Shurygin_S_max_po_stolbam_matrix/perf_tests/main.cpp
@@ -0,0 +1,77 @@
+﻿// Copyright 2023 Nesterov Alexander
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "core/perf/include/perf.hpp"
+#include "seq/Shurygin_S_max_po_stolbam_matrix/include/ops_seq.hpp"
+
+TEST(Shurygin_S_max_po_stolbam_matrix_seq_perf, test_pipeline_run) {
+  const int rows = 5000;
+  const int cols = 5000;
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  auto testTaskSequential = std::make_shared<Shurygin_S_max_po_stolbam_matrix_seq::TestTaskSequential>(taskDataSeq);
+  std::vector<std::vector<int>> matrix_rnd =
+      Shurygin_S_max_po_stolbam_matrix_seq::TestTaskSequential::generate_random_matrix(rows, cols);
+  for (auto& row : matrix_rnd) {
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(row.data()));
+  }
+  taskDataSeq->inputs_count.emplace_back(rows);
+  taskDataSeq->inputs_count.emplace_back(cols);
+  std::vector<int> v_res(cols, 0);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(v_res.data()));
+  taskDataSeq->outputs_count.emplace_back(v_res.size());
+
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
+  perfAnalyzer->pipeline_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+
+  for (int j = 0; j < cols; j++) {
+    ASSERT_EQ(v_res[j], 200);
+  }
+}
+
+TEST(Shurygin_S_max_po_stolbam_matrix_seq_perf, test_task_run) {
+  const int rows = 4560;
+  const int cols = 4560;
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  auto testTaskSequential = std::make_shared<Shurygin_S_max_po_stolbam_matrix_seq::TestTaskSequential>(taskDataSeq);
+  std::vector<std::vector<int>> matrix_rnd =
+      Shurygin_S_max_po_stolbam_matrix_seq::TestTaskSequential::generate_random_matrix(rows, cols);
+  for (auto& row : matrix_rnd) {
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(row.data()));
+  }
+  taskDataSeq->inputs_count.emplace_back(rows);
+  taskDataSeq->inputs_count.emplace_back(cols);
+  std::vector<int> v_res(cols, 0);
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(v_res.data()));
+  taskDataSeq->outputs_count.emplace_back(v_res.size());
+
+  auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
+  perfAttr->num_running = 10;
+  const auto t0 = std::chrono::high_resolution_clock::now();
+  perfAttr->current_timer = [&] {
+    auto current_time_point = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(current_time_point - t0).count();
+    return static_cast<double>(duration) * 1e-9;
+  };
+
+  auto perfResults = std::make_shared<ppc::core::PerfResults>();
+  auto perfAnalyzer = std::make_shared<ppc::core::Perf>(testTaskSequential);
+  perfAnalyzer->task_run(perfAttr, perfResults);
+  ppc::core::Perf::print_perf_statistic(perfResults);
+
+  for (int j = 0; j < cols; j++) {
+    ASSERT_EQ(v_res[j], 200);
+  }
+}
