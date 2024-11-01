df914fd0937c93e2c148b60be997a019f63fb252
Nesterov Alexander <nesterov.alexander@outlook.com>
2024-11-01 17:07:10 +0100

    Add separate running for debug tests (#106)

--- scripts/run_perf_collector.sh
+++ scripts/run_perf_collector.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
-if [[ -z "$ASAN_RUN" ]]; then
-  if [[ $OSTYPE == "linux-gnu" ]]; then
-    mpirun --oversubscribe -np 4 ./build/bin/mpi_perf_tests
-  elif [[ $OSTYPE == "darwin"* ]]; then
-    mpirun -np 2 ./build/bin/mpi_perf_tests
+# separate tests for debug
+for test_item in $(./build/bin/mpi_perf_tests --gtest_list_tests | awk '/\./{ SUITE=$1 }  /  / { print SUITE $1 }')
+do
+  if [[ -z "$ASAN_RUN" ]]; then
+    if [[ $OSTYPE == "linux-gnu" ]]; then
+      mpirun --oversubscribe -np 4 ./build/bin/mpi_perf_tests --gtest_filter="$test_item"
+    elif [[ $OSTYPE == "darwin"* ]]; then
+      mpirun -np 2 ./build/bin/mpi_perf_tests --gtest_filter="$test_item"
+    fi
   fi
-fi
+done
+
 ./build/bin/omp_perf_tests
 ./build/bin/seq_perf_tests
 ./build/bin/stl_perf_tests
