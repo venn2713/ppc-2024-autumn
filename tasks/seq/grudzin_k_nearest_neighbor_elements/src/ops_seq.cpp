12130c0cbbf52f5556ef693f00bc4784f07e71f1
Grudzin Konstantin <113104424+Konstantin-Grudzin@users.noreply.github.com>
2024-11-02 04:22:08 +0300

    Грудзин Константин. Задача 1. Вариант 7. Нахождение наиболее близких по значению соседних элементов вектора. (#54)
    **Основная идея**:
    Представим ответ в виде пары чисел - значения и самого индекса. Если мы
    по определению сравнения пар будем брать минимум, мы получим самый левый
    индекс с минимальным значением.
    
    **Последовательная реализация**:
    Мы проходимся по массиву и находим минимальный элемент среди пар
    "абсолютная разница соседних" и индексом.
    
    **Реализация MPI**:
    Мы разделяем наши массивы на несколько равных частей и отдаем каждому
    процессу свою часть массива.
    Он вычисляет минимум и с помощью операции reduce передает эту информацию
    главному процессу. Т.к мы проверяем ещё и следующий элемент, каждому
    процессу добавляем по одному дополнительному элементу. Чтобы избежать
    выход за массивы, а также корректно вычислять ответ, передаем каждому
    потоку точку его старта и размер действительных данных.

---
+++ tasks/seq/grudzin_k_nearest_neighbor_elements/src/ops_seq.cpp
@@ -0,0 +1,35 @@
+// Copyright 2024 Nesterov Alexander
+#include "seq/grudzin_k_nearest_neighbor_elements/include/ops_seq.hpp"
+
+#include <climits>
+
+bool grudzin_k_nearest_neighbor_elements_seq::TestTaskSequential::pre_processing() {
+  internal_order_test();
+  // Init value for input and output
+  input_ = std::vector<int>(taskData->inputs_count[0]);
+  auto* tmp_ptr = reinterpret_cast<int*>(taskData->inputs[0]);
+  std::copy(tmp_ptr, tmp_ptr + taskData->inputs_count[0], input_.begin());
+  // Init value for output
+  res = {INT_MAX, -1};
+  return true;
+}
+
+bool grudzin_k_nearest_neighbor_elements_seq::TestTaskSequential::validation() {
+  internal_order_test();
+  // Check count elements of output
+  return taskData->inputs_count[0] > 1 && taskData->outputs_count[0] == 1;
+}
+
+bool grudzin_k_nearest_neighbor_elements_seq::TestTaskSequential::run() {
+  internal_order_test();
+  for (size_t i = 0; i < input_.size() - 1; i++) {
+    res = std::min(res, {abs(input_[i] - input_[i + 1]), i});
+  }
+  return true;
+}
+
+bool grudzin_k_nearest_neighbor_elements_seq::TestTaskSequential::post_processing() {
+  internal_order_test();
+  reinterpret_cast<int*>(taskData->outputs[0])[0] = res.second;
+  return true;
+}
